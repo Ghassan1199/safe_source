e2f649527874f8f7d22c62c1d6f3615f33b39c8a
ghassan1199 <ghassanmah@gmail.com>
2023-11-19 12:31:18 +0300

    adding password to user

--- api/models/user.js
+++ api/models/user.js
@@ -1,16 +1,19 @@
 
-    
-    module.exports =(sequelize, DataTypes) => {
-        return sequelize.define("user",{
-            id:{
-                type: DataTypes.INTEGER,
-                autoIncrement: true,
-                primaryKey: true,
-            },
-            name:{
-                type: DataTypes.STRING,
-                unique: true,
-                allowNull: false
-            }
-        });
-    };
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("user", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+};
