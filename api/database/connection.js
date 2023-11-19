73af523953575cc4147aa556c7696f15edd23515
ghassan1199 <ghassanmah@gmail.com>
2023-11-19 11:34:45 +0300

    adding the env

--- api/database/connection.js
+++ api/database/connection.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 
-const sequelize = new Sequelize('postgres://postgres:1234@localhost:5432/source',{logging:false});
+const sequelize = new Sequelize(`${process.env.DATABASE}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{logging:false});
 
 
   module.exports = {sequelize, DataTypes};
